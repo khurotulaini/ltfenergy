41aad81b787be785600a350272267c054906b551
khurotul aini husna <aini9900933850@gmail.com>
2024-01-23 12:47:05 +0800

    update

--- message.php
+++ message.php
@@ -1,22 +1,18 @@
 <?php
-  $name = htmlspecialchars($_POST['name']);
-  $email = htmlspecialchars($_POST['email']);
-  $message = htmlspecialchars($_POST['message']);
+// Receive form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
-  if(!empty($email) && !empty($message)){
-    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-      $receiver = "chuahyongxie@gmail.com"; //enter that email address where you want to receive all messages
-      $subject = "From: $name <$email>";
-      $body = "Name: $name\nEmail: $email\nPhone: \n\n$message\n\nRegards,\n$name";
-      $sender = "From: $email";
-      if(mail($receiver, $subject, $body, $sender)){
-         echo "Your message has been sent";
-      }else{
-         echo "Sorry, failed to send your message!";
-      }
-    }else{
-      echo "Enter a valid email address!";
-    }
-  }else{
-    echo "Email and message field is required!";
-  }
+// Compose email
+$to = "chuahyongxie@gmail.com";
+$subject = "New Contact Form Submission";
+$body = "Name: $name\nEmail: $email\nMessage: $message";
+
+// Send email
+if (mail($to, $subject, $body)) {
+    echo "Message sent successfully!";
+} else {
+    echo "Error sending message.";
+}
+?>
